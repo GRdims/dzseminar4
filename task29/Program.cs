35853d4796564eed866b83625d8ca0065a3c5d39
Dmitriy <dgan0190@gmail.com>
2022-12-08 00:31:04 +0300

    task29 задание на вывод массива

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+int[] array = SetArray(8);
+Console.WriteLine($"{String.Join(",", array)} -> [{String.Join(",", array)}]");
+
+int[] SetArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-100, 100);
+    }
+
+    return result;
+}
